1edbaecff9bc410daa39c643c3604c7080b2c17c
AngLihenshuai <18629692129la@gmail.com>
2021-09-13 21:59:08 -0700

    add grade level

--- res/StuDb.js
+++ res/StuDb.js
@@ -328,3 +328,970 @@ var stuDb4_6 = [
     "prize": 4
   }
 ]
+var stuDb7_8 = [
+  {
+    "name": "Dhruv Viraya",
+    "project": "Force of Nature",
+    "prize": 0
+  },
+  {
+    "name": "David Zheng",
+    "project": "Edge of the Realm",
+    "prize": 1
+  },
+  {
+    "name": "Xinke Wang",
+    "project": "Metal Clash",
+    "prize": 1
+  },
+  {
+    "name": "Raymond Kuang",
+    "project": "Platformer game",
+    "prize": 1
+  },
+  {
+    "name": "Spencer Yuan",
+    "project": "Plane combined",
+    "prize": 1
+  },
+  {
+    "name": "Mathew Xie",
+    "project": "Notai",
+    "prize": 1
+  },
+  {
+    "name": "Melody Bao",
+    "project": "Space Invaders",
+    "prize": 2
+  },
+  {
+    "name": "Darren Huang",
+    "project": "9 rooms",
+    "prize": 2
+  },
+  {
+    "name": "Mingxin (Sissi) Yu",
+    "project": "Journey to the West",
+    "prize": 2
+  },
+  {
+    "name": "Athena Bi",
+    "project": "Archive of book games",
+    "prize": 2
+  },
+  {
+    "name": "Larry Zhou",
+    "project": "Gladiator Fight",
+    "prize": 2
+  },
+  {
+    "name": "Yantting Li",
+    "project": "Med Tracker and box",
+    "prize": 2
+  },
+  {
+    "name": "Zihao Lin",
+    "project": "Wild West Shooter",
+    "prize": 2
+  },
+  {
+    "name": "Tony-Tom Bai",
+    "project": "Vaccine vs virus",
+    "prize": 2
+  },
+  {
+    "name": "Anya Li",
+    "project": "FeedForFit",
+    "prize": 2
+  },
+  {
+    "name": "Jerry Li",
+    "project": "BloodSimpleScan",
+    "prize": 2
+  },
+  {
+    "name": "Grace Huang",
+    "project": "Selcouth(Unity)",
+    "prize": 2
+  },
+  {
+    "name": "Matthew Olen",
+    "project": "Ping Pong(Python)",
+    "prize": 2
+  },
+  {
+    "name": "Adrian Shou",
+    "project": "Python Snake Game",
+    "prize": 2
+  },
+  {
+    "name": "Thomas Yang",
+    "project": "Platform Game Peinguin",
+    "prize": 2
+  },
+  {
+    "name": "Nina Huang",
+    "project": "Virtual Pet",
+    "prize": 2
+  },
+  {
+    "name": "Siyou Wang",
+    "project": "Math RPG",
+    "prize": 2
+  },
+  {
+    "name": "Matt Chui",
+    "project": "The Game of Life",
+    "prize": 2
+  },
+  {
+    "name": "Xiaoke Pang",
+    "project": "Tic Tac Toe(Python)",
+    "prize": 2
+  },
+  {
+    "name": "Jonathan Yan",
+    "project": "Ultra Death Bot",
+    "prize": 3
+  },
+  {
+    "name": "Belinda Mao",
+    "project": "TravelerS",
+    "prize": 3
+  },
+  {
+    "name": "Qifeng (Jerry) Sun",
+    "project": "Obby",
+    "prize": 3
+  },
+  {
+    "name": "Isaac Tan",
+    "project": "Tic Tac Toe",
+    "prize": 3
+  },
+  {
+    "name": "Jiaheng Su",
+    "project": "Monopoly",
+    "prize": 3
+  },
+  {
+    "name": "Jerry Wang",
+    "project": "Egg Catch Game",
+    "prize": 3
+  },
+  {
+    "name": "Ivan Gao",
+    "project": "Shape Game",
+    "prize": 3
+  },
+  {
+    "name": "Kendrick Mui",
+    "project": "TA DA To Do Reminder",
+    "prize": 3
+  },
+  {
+    "name": "Tommy Xie",
+    "project": "5 Language Interest Calculator",
+    "prize": 3
+  },
+  {
+    "name": "Nate Zhou",
+    "project": "Game Simulator",
+    "prize": 3
+  },
+  {
+    "name": "Zachery Zhang",
+    "project": "Shooting game",
+    "prize": 3
+  },
+  {
+    "name": "Jason Zhang",
+    "project": "Ball roll game",
+    "prize": 3
+  },
+  {
+    "name": "Tiger Wang",
+    "project": "Pygame platformer",
+    "prize": 3
+  },
+  {
+    "name": "Anna Dai",
+    "project": "The productivity",
+    "prize": 3
+  },
+  {
+    "name": "Sean Ning",
+    "project": "RPG Game",
+    "prize": 3
+  },
+  {
+    "name": "Ethan Zhang",
+    "project": "A Really Long Obby (Roblox)",
+    "prize": 3
+  },
+  {
+    "name": "Zinuo Xu",
+    "project": "Maze Prize Finder",
+    "prize": 3
+  },
+  {
+    "name": "Jingxing Chen",
+    "project": "Handman",
+    "prize": 3
+  },
+  {
+    "name": "Alex Wen",
+    "project": "Calculator",
+    "prize": 3
+  },
+  {
+    "name": "Lucas Li",
+    "project": "Calories calculator",
+    "prize": 3
+  },
+  {
+    "name": "Tyler Chen",
+    "project": "Car Game",
+    "prize": 3
+  },
+  {
+    "name": "Victor Chen",
+    "project": "Glitch Generation(Unity)",
+    "prize": 3
+  },
+  {
+    "name": "Russel Qian",
+    "project": "Mimic RPG Game(Python)",
+    "prize": 4
+  },
+  {
+    "name": "Shuanghua (George) Yang",
+    "project": "Timer",
+    "prize": 4
+  },
+  {
+    "name": "Ryan Wang",
+    "project": "The little fan",
+    "prize": 4
+  },
+  {
+    "name": "Cody Duan",
+    "project": "Claw robot",
+    "prize": 4
+  },
+  {
+    "name": "Albert Teoh",
+    "project": "Gun Game",
+    "prize": 4
+  },
+  {
+    "name": "Kam Ching Wu",
+    "project": "Computer Fan(Hardware Python)",
+    "prize": 4
+  },
+  {
+    "name": "Nathan Hsu",
+    "project": "Water Level Reduction(Hardware Python)",
+    "prize": 4
+  },
+  {
+    "name": "Owen Chen",
+    "project": "Soil Moisture(Hardware Python)",
+    "prize": 4
+  },
+  {
+    "name": "Evangeline Dawson",
+    "project": "Virtual Sky",
+    "prize": 4
+  },
+  {
+    "name": "Alysha Winters",
+    "project": "AR Game",
+    "prize": 4
+  },
+  {
+    "name": "Janae Kearns",
+    "project": "Hunter Game",
+    "prize": 4
+  },
+  {
+    "name": "Issac Cowan",
+    "project": "Minecraft Maze",
+    "prize": 4
+  },
+  {
+    "name": "Patryk Duggan",
+    "project": "Encryption Test",
+    "prize": 4
+  },
+  {
+    "name": "Esa Norris",
+    "project": "Dog Tracker",
+    "prize": 4
+  },
+  {
+    "name": "Dakota Ford",
+    "project": "Forest Run",
+    "prize": 4
+  },
+  {
+    "name": "Naeem Read",
+    "project": "LifeSave",
+    "prize": 4
+  },
+  {
+    "name": "Elina Millington",
+    "project": "Hill Star",
+    "prize": 4
+  },
+  {
+    "name": "Jada Donaldson",
+    "project": "Music Play",
+    "prize": 4
+  }
+]
+var stuDb9_12_ai = [
+  {
+    "name": "Tianjiao Dong",
+    "project": "CatwalkGrader",
+    "prize": 1
+  },
+  {
+    "name": "Demi Li",
+    "project": "Judge4fencing",
+    "prize": 1
+  },
+  {
+    "name": "Shuo Chen",
+    "project": "Intelligent computer configuration recommendations",
+    "prize": 2
+  },
+  {
+    "name": "Shawn Liu",
+    "project": "SmartBark",
+    "prize": 2
+  },
+  {
+    "name": "MengCheng Han",
+    "project": "Car Detection",
+    "prize": 2
+  },
+  {
+    "name": "Aaron Zhu",
+    "project": "Useful Command Line interface",
+    "prize": 3
+  },
+  {
+    "name": "Jesse Chen",
+    "project": "Riddle Questions",
+    "prize": 3
+  },
+  {
+    "name": "Jay Qiu",
+    "project": "Market & Data Analysis Program",
+    "prize": 3
+  },
+  {
+    "name": "Hans Li",
+    "project": "Recognizing Pets' Emotions",
+    "prize": 3
+  },
+  {
+    "name": "Pak Fung Li",
+    "project": "Maze Game",
+    "prize": 3
+  },
+  {
+    "name": "Colin Yang",
+    "project": "Discord Bot",
+    "prize": 4
+  },
+  {
+    "name": "Rowan Kent",
+    "project": "Tensorflow in Medical",
+    "prize": 4
+  },
+  {
+    "name": "Rees Merritt",
+    "project": "AI-Sense",
+    "prize": 4
+  },
+  {
+    "name": "Sienna-Rose Reyna",
+    "project": "Weather Prediction",
+    "prize": 4
+  },
+  {
+    "name": "Malik Blake",
+    "project": "California Fire Visualization",
+    "prize": 4
+  },
+  {
+    "name": "Danni Serrano",
+    "project": "Smartdoor",
+    "prize": 4
+  }
+]
+var stuDb9_12_game = [
+  {
+    "name": "Angel Wu",
+    "project": "Adventure",
+    "prize": 1
+  },
+  {
+    "name": "Nathan Lee",
+    "project": "V Rebirth",
+    "prize": 1
+  },
+  {
+    "name": "Selina Gong",
+    "project": "Flory",
+    "prize": 1
+  },
+  {
+    "name": "Edward Liu",
+    "project": "RPG Game",
+    "prize": 1
+  },
+  {
+    "name": "Jonathan Li",
+    "project": "2D Pixel Fighter",
+    "prize": 2
+  },
+  {
+    "name": "Andy Jia",
+    "project": "Toewr Le Fence",
+    "prize": 2
+  },
+  {
+    "name": "Richard Feng",
+    "project": "Towers Pass",
+    "prize": 2
+  },
+  {
+    "name": "Kevin Hu",
+    "project": "Super Doge Galaxy",
+    "prize": 2
+  },
+  {
+    "name": "Zachary Liang",
+    "project": "Waterworld",
+    "prize": 2
+  },
+  {
+    "name": "Eiffel Vuong",
+    "project": "A Dark Room",
+    "prize": 2
+  },
+  {
+    "name": "Weichang (Jack) Wang",
+    "project": "Phymulation",
+    "prize": 2
+  },
+  {
+    "name": "Ethan Chen",
+    "project": "Particle Box",
+    "prize": 2
+  },
+  {
+    "name": "Yingzhi Ma",
+    "project": "X Base",
+    "prize": 2
+  },
+  {
+    "name": "Joshua Li",
+    "project": "Potatoes must die",
+    "prize": 2
+  },
+  {
+    "name": "Richard Yu",
+    "project": "Nocturn",
+    "prize": 2
+  },
+  {
+    "name": "Sean Zheng (not on main list)",
+    "project": "Pygame",
+    "prize": 2
+  },
+  {
+    "name": "Jie Zhou",
+    "project": "brain power",
+    "prize": 2
+  },
+  {
+    "name": "Jack Liu",
+    "project": "Merge Elements",
+    "prize": 3
+  },
+  {
+    "name": "Sam Gou",
+    "project": "Roblox",
+    "prize": 3
+  },
+  {
+    "name": "Sean Zhou",
+    "project": "WW2 Capture the flag",
+    "prize": 3
+  },
+  {
+    "name": "Gary Wang",
+    "project": "Dungeon Project",
+    "prize": 3
+  },
+  {
+    "name": "Michelle Wang",
+    "project": "History Quiz Game",
+    "prize": 3
+  },
+  {
+    "name": "Jerry Hu",
+    "project": "Fighting Fat Blob",
+    "prize": 3
+  },
+  {
+    "name": "Carina Zheng",
+    "project": "Dictionary",
+    "prize": 3
+  },
+  {
+    "name": "Congyu Zhao",
+    "project": "Unity FPS",
+    "prize": 3
+  },
+  {
+    "name": "Sophia Fu",
+    "project": "Python Turtle",
+    "prize": 3
+  },
+  {
+    "name": "Charlene Cheng",
+    "project": "Saving the Gold",
+    "prize": 3
+  },
+  {
+    "name": "Stephen Hung",
+    "project": "Fantasy RPG",
+    "prize": 3
+  },
+  {
+    "name": "Aidan Chen",
+    "project": "Mario-Inspired Platformer",
+    "prize": 3
+  },
+  {
+    "name": "Haolin Xie",
+    "project": "Spaceship cam",
+    "prize": 3
+  },
+  {
+    "name": "Jason Xiao",
+    "project": "Unity Platformer Project",
+    "prize": 3
+  },
+  {
+    "name": "Zixuan (Casson) Qin",
+    "project": "Color Picker",
+    "prize": 3
+  },
+  {
+    "name": "Kevin Zhou",
+    "project": "Desert Game",
+    "prize": 3
+  },
+  {
+    "name": "Xuanwei Ding",
+    "project": "The Hunted House",
+    "prize": 3
+  },
+  {
+    "name": "Zihan Zhao, Zhouyi Yang, Xiaoyue Huang",
+    "project": "Human Organ Quzzing",
+    "prize": 3
+  },
+  {
+    "name": "Edward Huang",
+    "project": "Zoom Adventure",
+    "prize": 3
+  },
+  {
+    "name": "Letitia Schmidt",
+    "project": "Lowland Space",
+    "prize": 4
+  },
+  {
+    "name": "Tasneem Tucker",
+    "project": "SimShop",
+    "prize": 4
+  },
+  {
+    "name": "Anam Mullins",
+    "project": "Racing for Speed",
+    "prize": 4
+  },
+  {
+    "name": "Garry Gutierrez",
+    "project": "Minecraft Mod",
+    "prize": 4
+  },
+  {
+    "name": "Leighton Pugh",
+    "project": "Mini Games",
+    "prize": 4
+  },
+  {
+    "name": "Kerri Simons",
+    "project": "MyGoals",
+    "prize": 4
+  },
+  {
+    "name": "Esmee James",
+    "project": "Text RPG",
+    "prize": 4
+  },
+  {
+    "name": "Suman Vance",
+    "project": "Finder",
+    "prize": 4
+  },
+  {
+    "name": "Norman Stewart",
+    "project": "Boy Scout Adventure",
+    "prize": 4
+  },
+  {
+    "name": "Matylda Macdonald",
+    "project": "Flight Simulator",
+    "prize": 4
+  }
+]
+var stuDb9_12_cyber = [
+  {
+    "name": "Katherine Hua",
+    "project": "AI and Ultrasonic Based Smart Device for the Visually Impaired",
+    "prize": 1
+  },
+  {
+    "name": "Erica Mao",
+    "project": "Water Bowl Level",
+    "prize": 2
+  },
+  {
+    "name": "James Jin",
+    "project": "Safelink",
+    "prize": 2
+  },
+  {
+    "name": "Guo Lin",
+    "project": "Cat Door Opener",
+    "prize": 2
+  },
+  {
+    "name": "Annabella Luo",
+    "project": "Water Level Detection Sensor",
+    "prize": 3
+  },
+  {
+    "name": "Benjamin Luo",
+    "project": "Water Level Detection Sensor",
+    "prize": 3
+  },
+  {
+    "name": "Arthur Liang",
+    "project": "Perfect Shape Sketcher",
+    "prize": 3
+  },
+  {
+    "name": "Ajay Sexton",
+    "project": "DDoS Defense",
+    "prize": 4
+  },
+  {
+    "name": "Chandni Edwards",
+    "project": "Cyber AIM",
+    "prize": 4
+  },
+  {
+    "name": "Billie Kaufman",
+    "project": "Smart Switch",
+    "prize": 4
+  },
+  {
+    "name": "Verity Fuller",
+    "project": "My Router",
+    "prize": 4
+  }
+]
+var stuDb9_12_mobile = [
+  {
+    "name": "Jason Wu",
+    "project": "Playguessr",
+    "prize": 1
+  },
+  {
+    "name": "Julius-Jerry Wu-Wang",
+    "project": "Football Tracking Device",
+    "prize": 1
+  },
+  {
+    "name": "Nina Luo",
+    "project": "Dishcovery",
+    "prize": 1
+  },
+  {
+    "name": "Charles Jin",
+    "project": "Cougar Book",
+    "prize": 2
+  },
+  {
+    "name": "Thomas Xiao",
+    "project": "Sleepy Eyes",
+    "prize": 2
+  },
+  {
+    "name": "Harrisson Li",
+    "project": "The Real Shopping Ranks",
+    "prize": 2
+  },
+  {
+    "name": "XuXuan (Shania) Jin",
+    "project": "Music Sheet",
+    "prize": 2
+  },
+  {
+    "name": "Serena Wen",
+    "project": "Volonteer coordination platform.",
+    "prize": 2
+  },
+  {
+    "name": "Ruoxi (Alivia) Ding",
+    "project": "Instagram Depression Detection",
+    "prize": 2
+  },
+  {
+    "name": "Maggie Ding",
+    "project": "Nutrition App",
+    "prize": 2
+  },
+  {
+    "name": "Aaron Fei",
+    "project": "Forecasting Extended Warranty for Manufacturing Company",
+    "prize": 2
+  },
+  {
+    "name": "Michael Li",
+    "project": "FitConnect",
+    "prize": 3
+  },
+  {
+    "name": "Nicole Ma",
+    "project": "LesionVision",
+    "prize": 3
+  },
+  {
+    "name": "Jevon Mao",
+    "project": "SMHS: School App",
+    "prize": 3
+  },
+  {
+    "name": "Haobo Sun",
+    "project": "Discord Smart Bot",
+    "prize": 3
+  },
+  {
+    "name": "Nick Li and Annie Zhang",
+    "project": "Plant Disease Detector App",
+    "prize": 3
+  },
+  {
+    "name": "Steve Wu, Bright Li, Az Li, Johnny Wu, Kathy Hua",
+    "project": "Math matters",
+    "prize": 3
+  },
+  {
+    "name": "Allen Weng",
+    "project": "HistoryScope",
+    "prize": 3
+  },
+  {
+    "name": "Leo Duan",
+    "project": "HyPixel Store",
+    "prize": 3
+  },
+  {
+    "name": "Emaan Simmons",
+    "project": "Scheduler Finder",
+    "prize": 4
+  },
+  {
+    "name": "Ivan May",
+    "project": "My Fav",
+    "prize": 4
+  },
+  {
+    "name": "Aaliya Lara",
+    "project": "EventHub",
+    "prize": 4
+  },
+  {
+    "name": "Ayda Parsons",
+    "project": "Planner App",
+    "prize": 4
+  },
+  {
+    "name": "Kaylem Patterson",
+    "project": "My Availalbility",
+    "prize": 4
+  },
+  {
+    "name": "Kyra Livingston",
+    "project": "Homepage Web",
+    "prize": 4
+  },
+  {
+    "name": "Evie-Mae Barnett",
+    "project": "RecipeApp",
+    "prize": 4
+  },
+  {
+    "name": "Jenna Acosta",
+    "project": "Nav4Time",
+    "prize": 4
+  }
+]
+var stuDb9_12_application = [
+  {
+    "name": "Qian (Erica) Zhang",
+    "project": "Humidity Conditioner",
+    "prize": 1
+  },
+  {
+    "name": "Justin Li",
+    "project": "Deep Fusion",
+    "prize": 1
+  },
+  {
+    "name": "Yuyang Lou",
+    "project": "Edu-Vision",
+    "prize": 2
+  },
+  {
+    "name": "Harrisson Li",
+    "project": "E-Dictionary",
+    "prize": 2
+  },
+  {
+    "name": "Leo Liao",
+    "project": "Git Freight",
+    "prize": 2
+  },
+  {
+    "name": "Cobbi Liu",
+    "project": "Enly",
+    "prize": 2
+  },
+  {
+    "name": "Brian Hu",
+    "project": "Smart Tab Predictor",
+    "prize": 2
+  },
+  {
+    "name": "Yilin Chen",
+    "project": "Tutorial4Senior",
+    "prize": 2
+  },
+  {
+    "name": "David Tang",
+    "project": "3DTetris",
+    "prize": 2
+  },
+  {
+    "name": "Ethan Loh",
+    "project": "Task Switcher",
+    "prize": 3
+  },
+  {
+    "name": "Aaron Tse",
+    "project": "Sun Tzu - Discord Bot",
+    "prize": 3
+  },
+  {
+    "name": "Sherry Chen",
+    "project": "Affly",
+    "prize": 3
+  },
+  {
+    "name": "Andrew Zhou",
+    "project": "New Doom",
+    "prize": 3
+  },
+  {
+    "name": "Leon Tian",
+    "project": "ParkourMaker",
+    "prize": 3
+  },
+  {
+    "name": "Leheng Huang",
+    "project": "History Discord Bot",
+    "prize": 3
+  },
+  {
+    "name": "Zhenwei Yang",
+    "project": "Music Recommendation",
+    "prize": 3
+  },
+  {
+    "name": "Xiangyu Wang",
+    "project": "Recommendation Engine (Movies)",
+    "prize": 3
+  },
+  {
+    "name": "Anna Ye",
+    "project": "Covid-19 Data Visualization",
+    "prize": 3
+  },
+  {
+    "name": "Cathie Zhu",
+    "project": "Groceries Delivey",
+    "prize": 3
+  },
+  {
+    "name": "Fiona Peng",
+    "project": "Food Interest",
+    "prize": 4
+  },
+  {
+    "name": "Yuanzhi (Patrick) Zhao",
+    "project": "Waste/Recycle Data Visualization",
+    "prize": 4
+  },
+  {
+    "name": "Mingfeng Liang",
+    "project": "Recommendation_Engine",
+    "prize": 4
+  },
+  {
+    "name": "Nichole Milne",
+    "project": "Stay Focus",
+    "prize": 4
+  },
+  {
+    "name": "Jevon Watt",
+    "project": "Science4All",
+    "prize": 4
+  },
+  {
+    "name": "Akeem Hebert",
+    "project": "Report Generator",
+    "prize": 4
+  },
+  {
+    "name": "Maddison Valdez",
+    "project": "JavaEdu",
+    "prize": 4
+  },
+  {
+    "name": "Lorna Bullock",
+    "project": "Countdown App",
+    "prize": 4
+  }
+]
--- res/res-all.js
+++ res/res-all.js
@@ -9,21 +9,42 @@ const tableList = ["result_table_best", "result_table_first", "result_table_seco
 const statisticList = {
 	'k-3': '<thead><tr class="table-success"><th scope="col">Total Participates</th><th scope="col">Total School</th><th scope="col">Grade K</th><th colspan="col">Grade 1</th><th scope="col">Grade 2</th><th scope="col">Grade 3</th></tr></thead>' + '<tr><td>101</td><td>39</td><td>11</td><td>25</td><td>26</td><td>39</td></tr>',
 	'4-6': '<thead><tr class="table-success"><th scope="col">Total Participates</th><th scope="col">Total School</th><th scope="col">Grade 4</th><th colspan="col">Grade 5</th><th scope="col">Grade 6</th></tr></thead>' + '<tr><td>110</td><td>62</td><td>31</td><td>33</td><td>46</td></tr>',
+	'7-8': '<thead><tr class="table-success"><th scope="col">Total Participates</th><th scope="col">Total School</th><th scope="col">Grade 7</th><th colspan="col">Grade 8</th></tr></thead>' + '<tr><td>131</td><td>73</td><td>55</td><td>76</td></tr>',
+	'9-12-ai': '<thead><tr class="table-success"><th scope="col">Total Participates</th><th scope="col">Total School</th><th scope="col">Grade 9</th><th colspan="col">Grade 10</th><th colspan="col">Grade 11</th><th colspan="col">Grade 12</th></tr></thead>' + '<tr><td>181</td><td>68</td><td>39</td><td>42</td><td>60</td><td>40</td></tr>',
+	'9-12-game': '<thead><tr class="table-success"><th scope="col">Total Participates</th><th scope="col">Total School</th><th scope="col">Grade 9</th><th colspan="col">Grade 10</th><th colspan="col">Grade 11</th><th colspan="col">Grade 12</th></tr></thead>' + '<tr><td>261</td><td>155</td><td>77</td><td>69</td><td>60</td><td>65</td></tr>',
+	'9-12-cyber': '<thead><tr class="table-success"><th scope="col">Total Participates</th><th scope="col">Total School</th><th scope="col">Grade 9</th><th colspan="col">Grade 10</th><th colspan="col">Grade 11</th><th colspan="col">Grade 12</th></tr></thead>' + '<tr><td>88</td><td>46</td><td>20</td><td>32</td><td>21</td><td>15</td></tr>',
+	'9-12-mobile': '<thead><tr class="table-success"><th scope="col">Total Participates</th><th scope="col">Total School</th><th scope="col">Grade 9</th><th colspan="col">Grade 10</th><th colspan="col">Grade 11</th><th colspan="col">Grade 12</th></tr></thead>' + '<tr><td>109</td><td>68</td><td>31</td><td>52</td><td>16</td><td>20</td></tr>',
+	'9-12-application': '<thead><tr class="table-success"><th scope="col">Total Participates</th><th scope="col">Total School</th><th scope="col">Grade 9</th><th colspan="col">Grade 10</th><th colspan="col">Grade 11</th><th colspan="col">Grade 12</th></tr></thead>' + '<tr><td>150</td><td>79</td><td>36</td><td>44</td><td>36</td><td>34</td></tr>',
 }
 const levelList = {
 	'k-3': 'Grade K - 3',
 	'4-6': 'Grade 4 - 6',
-	//'7-8': stuDb7_8,
-	//'9-12': stuDb9_12
+	'7-8': 'Grade 7 - 8',
+	'9-12-ai': 'Grade 9 - 12\nAI, Data Science and Algorithm',
+	'9-12-game': 'Grade 9 - 12\nGame Design',
+	'9-12-cyber': 'Grade 9 - 12\nCyber Security',
+	'9-12-mobile': 'Grade 9 - 12\nMoblie and Web',
+	'9-12-application': 'Grade 9 - 12\nSystem Application'
 }
 const dbList = {
 	'k-3': stuDbk_3,
 	'4-6': stuDb4_6,
-	//'7-8': stuDb7_8,
-	//'9-12': stuDb9_12
+	'7-8': stuDb7_8,
+	'9-12-ai': stuDb9_12_ai,
+	'9-12-game': stuDb9_12_game,
+	'9-12-cyber': stuDb9_12_cyber,
+	'9-12-mobile': stuDb9_12_mobile,
+	'9-12-application': stuDb9_12_application
+
 }
 var stuDb = dbList[group]
 console.log(stuDb)
+if (group == 'k-3' || group == '4-6' || group == '7-8') {
+	console.log("Have best");
+} else {
+	var t = document.getElementById("best_project")
+	t.style.display = "none";
+}
 document.getElementById("gradeLevel").innerHTML = levelList[group]
 document.getElementById("statistic_table").innerHTML = statisticList[group]
 
